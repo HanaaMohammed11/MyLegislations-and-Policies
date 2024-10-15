16bf2c9604c91dc11458f869d67175755aebb33a
YoussefFathy6 <yousseffathy20@gmail.com>
2024-10-15 16:44:21 +0300

    firebase added

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -11,12 +11,12 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAfgNyCtt0j6m_Yqb_aWy7AR6e3IFWvxao",
-  authDomain: "masfofa-b2835.firebaseapp.com",
-  projectId: "masfofa-b2835",
-  storageBucket: "masfofa-b2835.appspot.com",
-  messagingSenderId: "687917925034",
-  appId: "1:687917925034:web:2f614fb6a3a5b9ccc406d3",
+  apiKey: "AIzaSyAihjm94opoP-p8a-TmLi44AfK2z6CCHSM",
+  authDomain: "mylegislations-and-policies.firebaseapp.com",
+  projectId: "mylegislations-and-policies",
+  storageBucket: "mylegislations-and-policies.appspot.com",
+  messagingSenderId: "633854784630",
+  appId: "1:633854784630:web:8ac5d6f0d7090965209b57",
 };
 
 // Initialize Firebase
